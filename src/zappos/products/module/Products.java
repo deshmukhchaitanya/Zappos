0b247c2570c1d4e9bce56bda1b7831cbc510f4f7
deshmukhchaitanya <chaitanya_deshmukh@yahoo.com>
2014-03-02 15:21:49 -0700

    commit

--- src/zappos/products/module/Products.java
+++ src/zappos/products/module/Products.java
@@ -6,17 +6,6 @@ public class Products
 	String productName;
 	String productId;        // considering products have unique product Id 
 	
-//	@Override
-//	public boolean equals(Object obj) 
-//	{
-//		Products p=(Products)obj;
-//		return (this.getProductId()==p.getProductId());
-//	}
-//	@Override
-//	public int hashCode() {
-//	
-//		return Integer.parseInt(getProductId());
-//	}
 	public Double getDoublePrice() {
 		return Double.parseDouble(price.substring(1));
 	}
